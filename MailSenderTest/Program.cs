a9b045954e0544f94b3aa92ec2ec5074a12e11fa
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-06-17 18:31:46 +0300

    ДЗ Урок 1

---
+++ MailSenderTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSenderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var clinet = new SmtpClient("smtp.yandex.ru", 25)
+            {
+                Credentials = new NetworkCredential("UserName", "Password"),
+                EnableSsl = true
+            })
+            {
+                using(var msg = new MailMessage
+                {
+                    From = new MailAddress("sender@yandex.ru", "Отправитель"),
+                    To =
+                    {
+                        new MailAddress("recipient@gmail.com", "Получатель")
+                    },
+                    Subject = "Тема письма",
+                    Body = "Тело письма",
+                    Attachments =
+                    {
+                        new Attachment("FlieName.zip")
+                    }
+                })
+                {
+                    clinet.Send(msg);
+                }
+            }
+
+            //clinet.Dispose();
+            //msg.Dispose();
+        }
+    }
+}
