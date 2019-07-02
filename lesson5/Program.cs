69626910b7e8cfd60c60f5833ea3b3e56591bd76
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-07-02 14:57:11 +0300

    Урок 5: ЗД выполнено. Проект lesson5.

--- lesson5/Program.cs
+++ lesson5/Program.cs
@@ -11,11 +11,42 @@ namespace lesson5
     {
         static void Main(string[] args)
         {
+            // Задание 1:
+            //WorkWithNumber();
+
+            // Задание 2: Написать приложение, 
+            // выполняющее парсинг CSV-файла произвольной структуры 
+            // и сохраняющее его в обычный TXT-файл.
+            // Все операции проходят в потоках. 
+            // CSV-файл заведомо имеет большой объём.
+
+            string path_file = "..\\..\\test_data\\";
+            string csv_file = "tmp";
+            char separator = '#';
+            CsvToTxt csvToTxt = new CsvToTxt($"{path_file}{csv_file}.csv", $"{path_file}converted_{csv_file}.txt", separator);
+
+            int count_lines = csvToTxt.LinesInCsv();
+
+            Console.WriteLine("Для начала конвертации нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.WriteLine($"Конвертация {count_lines} строк в исходном csv-файле запущена.");
+
+            Thread thread_txt = new Thread(() => csvToTxt.ToTxt());
+            thread_txt.Start();
+            thread_txt.Join();
+
+            Console.WriteLine($"Конвертация завершена. Переписано: {csvToTxt.LinesInTxt()} строк.");
+            Console.Write("Для выхода нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+        private static void WorkWithNumber()
+        {
             // Задание 1: 
             // а) факториал числа N, которое вводится с клавиатуру
             // b. сумму целых чисел до N, которое также вводится с клавиатуры
             Console.Write("Введите число > 0: ");
-            while(int.TryParse(Console.ReadLine(), out int num))
+            while (int.TryParse(Console.ReadLine(), out int num))
             {
                 var internal_num = num;
                 var thread1 = new Thread(() => Console.WriteLine($"Факториал {internal_num} = {factorial.GetFactorial(internal_num)}"));
