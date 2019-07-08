bfc79d0e81f8f317f016b144db8613f198da04e2
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-07-08 23:09:15 +0300

    Начало работы с EF

--- MailSenderTest/Program.cs
+++ MailSenderTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MailSenderTest.data;
+using System;
+using System.Linq;
 using System.Threading;
 
 namespace MailSenderTest
@@ -7,22 +9,54 @@ namespace MailSenderTest
     {
         static void Main(string[] args)
         {
+            using(var db = new data.SongsDb())
+            {
+                Console.WriteLine("Songs count - {0}",db.Songs.Count());
+            }
+
+            using( var db = new SongsDb())
+            {
+                if (!db.Songs.Any())
+                {
+                    var songs = new Song[100];
+                    for (int i = 0; i < songs.Length; i++)
+                    {
+                        songs[i] = new Song
+                        {
+                            Name = $"Песня {i + 1}",
+                            Artist = new Artist { Name = $"Исполнитель {i}" }
+                        };
+                    }
+                    db.Songs.AddRange(songs);
+                    db.SaveChanges();
+                }
+            }
+
+            using (var db = new data.SongsDb())
+            {
+                foreach (var item in db.Songs)
+                {
+                    Console.WriteLine($"Song {item.Name} - Artist {item.Artist.Name}");
+                }
+            }
+
             //ThreadTest.Start();
 
             //ThreadSyncronizationTest.Start();
 
-            var threads = new Thread[10];
-            for (var i = 0; i < threads.Length; i++)
-            {
-                int i0 = i;
-                threads[i] = new Thread(() => Console.WriteLine($"Сообщение №{i0}"));
-            }
-            for (var i = 0; i < threads.Length; i++)
-            {
-                threads[i].Start();
-            }
+            //var threads = new Thread[10];
+            //for (var i = 0; i < threads.Length; i++)
+            //{
+            //    int i0 = i;
+            //    threads[i] = new Thread(() => Console.WriteLine($"Сообщение №{i0}"));
+            //}
+            //for (var i = 0; i < threads.Length; i++)
+            //{
+            //    threads[i].Start();
+            //}
 
-            Console.ReadLine();
+            Console.WriteLine("Для завершения нажмите любую кнопку...");
+            Console.ReadKey();
         }
 
         
---
+++ MailSenderTest/data/SongsDb.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSenderTest.data
+{
+    class SongsDb : DbContext
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public SongsDb() : this("Name=SongsDb") { }
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="ConnectionString">Имя строки подключения из App.config или строка подключенияк БД</param>
+        public SongsDb(string ConnectionString) : base(ConnectionString)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+    }
+
+    [Table("Songs")]
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual Artist Artist { get; set; } //virtual - навигационное свойство, данные будут загружаться автоматически и из таблицы Artists
+    }
+
+    //[Table("Artists")]
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
