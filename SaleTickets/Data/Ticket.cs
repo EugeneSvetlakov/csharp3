7cd02812d2c6e1091df5f553e0e3cfb21a849e2c
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-07-09 15:33:38 +0300

    ДЗ Урок 7: 1) реализовано; 2-4) Положено начало, уперся в реализацию Интерфейса для EF
    Переделан класс реализующий Расчет перемножения матриц - посмотрите, корректно ли реализован Асинхронный подход.

---
+++ SaleTickets/Data/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using SaleTickets.Services;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SaleTickets.Data
+{
+    [Table("Tickets")]
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string SeanceName { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
---
+++ SaleTickets/Services/ITicketDataService.cs
@@ -0,0 +1,11 @@
+﻿using SaleTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaleTickets.Services
+{
+    public interface ITicketDataService<T> : IEntityFrameworkService<T> where T : Ticket { }
+}
